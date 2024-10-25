fe98f4105632e63f0f4a7efce1cb71de598fd1d3
ldelnidoher <lucia.delnido@ua.es>
2024-10-25 10:37:30 +0200

    Update form.py

--- form.py
+++ form.py
@@ -25,18 +25,18 @@ qavalue = st.text_input('Correo')
     else:
         with st.form('input_form'):
             qavalue = st.text_input('Correo')
-            if qavalue in results_option['Correo']:
-                st.text("Ya has registrado una respuesta")
-            else:
-                c = st.radio("Opción 1:",("A","B","C"), horizontal = True)
-                if c == "A":
-                    travalue = 2
-                if c == "B":
-                    travalue = 7
-                if 2 == "C":
-                    travalue = 2
-                trvalue = st.number_input('Opción 2:')
-                clickSubmit = st.form_submit_button('Submit')
+                if qavalue in results_option['Correo']:
+                    st.text("Ya has registrado una respuesta")
+                else:
+                    c = st.radio("Opción 1:",("A","B","C"), horizontal = True)
+                    if c == "A":
+                        travalue = 2
+                    if c == "B":
+                        travalue = 7
+                    if 2 == "C":
+                        travalue = 2
+                    trvalue = st.number_input('Opción 2:')
+                    clickSubmit = st.form_submit_button('Submit')
 
     if clickSubmit:
         results_option.loc[len(results_option)] = [ qavalue, travalue, trvalue]
