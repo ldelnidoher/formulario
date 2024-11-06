62a1b9e7676a12688cd1efca556b4757fa818c37
ldelnidoher <lucia.delnido@ua.es>
2024-11-06 13:48:45 +0100

    Update form.py

--- form.py
+++ form.py
@@ -3,7 +3,35 @@ import datetime
 import pandas as pd
 import numpy as np
 import streamlit as st
-
-
+import random as r
 
 st.date_input('fecha', help = "click on the week that you're interested in", value = None)
+
+t = datetime.datetime.today()
+day_of_week = t.isoweekday()
+suffix = t.strftime('%Y%m%d')
+ 
+csv_file = "files.csv"
+if os.path.exists(csv_file):
+    files = pd.read_csv(csv_file,index_col = False)
+else:
+    files = pd.DataFrame(
+        {'a':np.array([]),
+         'b':np.array([]),
+        }
+    )
+
+num1 = [r.random() for i in range(10)]
+num2 = [2+r.random() for i in range(10)]
+if not str(suffix)+'1.txt' in files['a'].values:
+  np.savetext(str(suffix)+'1.txt',num1,delimeter = ' \t')
+  np.savetxt(str(suffix)+'2.txt',num2,delimeter = ' \t')
+  files.iloc[len(files)] = [str(suffix)+'1.txt',str(suffix)+'2.txt']
+  st.file_uploader(str(suffix)+'1.txt')
+  st.file_uploader(str(suffix)+'2.txt')
+  
+st.dataframe(files)
+
+
+  
+  
