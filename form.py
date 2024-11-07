5c3a7832bd95c564d2a4965fbdc0cb9fa2c5e9fb
ldelnidoher <lucia.delnido@ua.es>
2024-11-07 09:01:00 +0100

    Update form.py

--- form.py
+++ form.py
@@ -35,11 +35,11 @@ if not str(suffix)+'1.txt' in files['a'].values:
   b = np.savetxt(n2,np.array(num2),fmt = '% 1.5f', delimiter = ' \t')
   files.loc[len(files.index)] = [str(suffix)+'1.txt',str(suffix)+'2.txt']
 
- with open(os.path.join("tempDir",n1),"wb") as f:
-    f.write(n.getbuffer())
+  with open(os.path.join("tempDir",n1),"wb") as f:
+     f.write(n.getbuffer())
 
- with open(os.path.join("tempDir",n2),"wb") as f:
-    f.write(n.getbuffer())
+  with open(os.path.join("tempDir",n2),"wb") as f:
+     f.write(n.getbuffer())
       
 st.dataframe(pd.read_csv(n1))
   
